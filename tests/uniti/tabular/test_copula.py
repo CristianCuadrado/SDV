ca073e6cc0f4244d7ecc15521b9a504756e4879e
CristianCuadrado <c.cuadrado91@gmail.com>
2020-08-06 07:34:32 +0100

    First version uniti testing

---
+++ tests/uniti/tabular/test_copula.py
@@ -0,0 +1,52 @@
+"""Tests for the sdv.models.utils module."""
+from unittest.mock import patch
+
+import numpy as np
+import pandas as pd
+import pytest
+
+from sdv.tabular.ctgan import  GaussianCopula
+  
+
+	def test__update_metadata(self):
+	    # METHOD: _update_metadata(self)
+	    # VALIDATE:
+	    #     * returned values
+	    #     * check the case whete len(scores) < self.k
+	    
+	def _fit(self, data):
+	    # METHOD: _update_metadata(self)
+	    # VALIDATE:
+	    #     * returned values
+	    #     * check the case whete len(scores) < self.k
+
+	def _sample(self, num_rows):
+	    # METHOD: _update_metadata(self)
+	    # VALIDATE:
+	    #     * returned values
+	    #     * check the case whete len(scores) < self.k
+
+	def get_parameters(self, flatten=False):
+	    # METHOD: _update_metadata(self)
+	    # VALIDATE:
+	    #     * returned values
+	    #     * check the case whete len(scores) < self.k
+
+	def _prepare_sampled_covariance(self, covariance):
+	    # METHOD: _update_metadata(self)
+	    # VALIDATE:
+	    #     * returned values
+	    #     * check the case whete len(scores) < self.k
+
+	def _unflatten_gaussian_copula(self, model_parameters):
+	    # METHOD: _update_metadata(self)
+	    # VALIDATE:
+	    #     * returned values
+	    #     * check the case whete len(scores) < self.k
+
+	def test__set_parameters(self, parameters, unflatten=False):
+            # METHOD: set_parameters(self, parameters, unflatten=False)
+	    # VALIDATE:
+	    #     * returned values
+	    #     * check the case whete len(scores) < self.k
+
---
+++ tests/uniti/tabular/test_ctgan.py
@@ -0,0 +1,14 @@
+"""Tests for the sdv.models.utils module."""
+from unittest.mock import patch
+
+import numpy as np
+import pandas as pd
+import pytest
+
+from sdv.tabular.ctgan import  CTGAN
+  
+
+def test_fit():
+
+
+def test_sample():
